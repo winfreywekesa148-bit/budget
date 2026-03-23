8f64872856914fa3f803d77e2b82a76c258aa41a
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-03-23 18:19:57 +0300

    Second function to covert all string to uppercase

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function calculateTax(amount) {
     const taxRate = 0.1;
     return amount * taxRate;
 }
+
+// convertToUpperCase
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
